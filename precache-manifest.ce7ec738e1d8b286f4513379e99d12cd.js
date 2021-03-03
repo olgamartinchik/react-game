684b8398180d5613710c5c9e98df4e0227008995
olgamartinchik <sintcerova@mail.ru>
2021-03-03 23:21:59 +0300

    Updates

---
+++ precache-manifest.ce7ec738e1d8b286f4513379e99d12cd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "a3c84d202003aa0ce785",
+    "url": "/react-game/static/css/main.442e50d1.chunk.css"
+  },
+  {
+    "revision": "a3c84d202003aa0ce785",
+    "url": "/react-game/static/js/main.dd317e33.chunk.js"
+  },
+  {
+    "revision": "4f184c02109fbdd76c1f",
+    "url": "/react-game/static/js/runtime~main.897fcb33.js"
+  },
+  {
+    "revision": "c1d6ed61eb4aa4ab9296",
+    "url": "/react-game/static/css/2.e1d167d5.chunk.css"
+  },
+  {
+    "revision": "c1d6ed61eb4aa4ab9296",
+    "url": "/react-game/static/js/2.aeefec63.chunk.js"
+  },
+  {
+    "revision": "af1a4ed3bbc31de95802",
+    "url": "/react-game/static/js/3.1c7aeab8.chunk.js"
+  },
+  {
+    "revision": "041b650c739a2cdba7fb791f7132d358",
+    "url": "/react-game/index.html"
+  }
+];
